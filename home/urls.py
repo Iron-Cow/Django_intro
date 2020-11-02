0484bcb0b5053e19e573a4497c01f92d93f96d9f
Iron-Cow <uangeji@gmail.com>
2020-11-02 22:05:37 +0200

    jinja2 templates learned

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index, htmltrain
+from .views import index, htmltrain, multipage, sign_in, register
 
 urlpatterns = [
-    path('', index),
-    # path('home', index)
-    path('htmltrain', htmltrain)
+    path('', index, name='homepage'),
+    path('html_train', htmltrain, name='htmltrain'),
+    path('multi_page/<int:page_id>', multipage, name='multipage'),
+    path('login', sign_in, name='login-page'),
+    path('register', register, name='register')
 ]
--- home/views.py
+++ home/views.py
@@ -14,3 +14,22 @@ def index(request):
 
 def htmltrain(request):
     return render(request, 'home/html-train.html')
+
+
+def multipage(request, page_id: int):
+    print(page_id)
+    data = {
+        'page__id': page_id,
+        'text': 'hello to you',
+        'animals': ['cat', 'dog', 'owl', 'fish'],
+        'range': range(10)
+    }
+    return render(request, 'home/multi-value-page.html', context=data)
+
+
+def sign_in(request):
+    return render(request, 'home/login.html')
+
+
+def register(request):
+    return render(request, 'home/register.html')
