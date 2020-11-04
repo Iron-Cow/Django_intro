7d93a4eabbbdb7bd8dc32892782c41edf11c4e23
Iron-Cow <uangeji@gmail.com>
2020-11-04 22:08:21 +0200

    registration \ authorization finished

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index, htmltrain, multipage, sign_in, register
+from .views import index, htmltrain, multipage, sign_in, register, logout_user
 
 urlpatterns = [
     path('', index, name='homepage'),
     path('html_train', htmltrain, name='htmltrain'),
     path('multi_page/<int:page_id>', multipage, name='multipage'),
     path('login', sign_in, name='login-page'),
-    path('register', register, name='register')
+    path('register', register, name='register'),
+    path('logout', logout_user, name='logout'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
@@ -28,8 +30,59 @@ def multipage(request, page_id: int):
 
 
 def sign_in(request):
-    return render(request, 'home/login.html')
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect("/")
+        return render(request, 'home/login.html')
+    elif request.method == "POST":
+        data = {}
+
+        _login = request.POST.get('login_field')
+        _password = request.POST.get('password_field')
+
+        user = authenticate(request, username=_login, password=_password)
+        if user is None:
+            data['report'] = 'Пользователь не найден или неверный пароль!'
+            return render(request, 'home/login.html', context=data)
+        else:
+            login(request, user)
+            return redirect("/")
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("/")
 
 
 def register(request):
-    return render(request, 'home/register.html')
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect("/")
+        return render(request, 'home/register.html', context={})
+    elif request.method == "POST":
+        login = request.POST.get('login_field')
+        email = request.POST.get('email_field')
+        pass1 = request.POST.get('password_field')
+        pass2 = request.POST.get('password_confirm_field')
+
+        data = dict()
+        data['login'] = login
+        data['email'] = email
+        data['pass1'] = pass1
+        data['pass2'] = pass2
+
+        if pass1 != pass2:
+            report = 'Пароли должны совпадать'
+        elif '' in data.values():
+            report = 'Все поля - ОБЯЗАТЕЛЬНЫЕ'
+        elif len(pass1) < 8:
+            report = 'Слишком короткий пароль'
+        else:
+            user = User.objects.create_user(login, email, pass1)
+            user.save()
+            if user:
+                return redirect("/")
+        report = 'Что-то страшное произошло!'
+        data['report'] = report
+        return render(request, 'home/register.html', context=data)
+
